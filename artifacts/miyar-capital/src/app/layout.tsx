10d55a02e447920e037201571a3b5a8bfbf827f4
adnan-businessanalyst <adnan.akhonbay@gmail.com>
2026-08-21 03:37:07 +0300

    Add Metropolis as the self-hosted English type family.

--- artifacts/miyar-capital/src/app/layout.tsx
+++ artifacts/miyar-capital/src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,400;0,500;0,600;1,400&family=DM+Sans:opsz,wght@9..40,300;9..40,400;9..40,500;9..40,600&family=IBM+Plex+Sans+Arabic:wght@400;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Arabic:wght@400;600;700&display=swap"
           rel="stylesheet"
         />
       </head>
